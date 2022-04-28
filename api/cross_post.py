8b3d80f34e3585c4a6f9c6f43c1d562cddb99726
levch <levchenko.d.a1998@gmail.com>
2022-04-29 00:27:12 +0300

    add Dockerfile

--- api/cross_post.py
+++ api/cross_post.py
@@ -6,8 +6,10 @@ from models import documents
 
 async def send_edited_copy_of_msg(msg: types.Message, route: documents.Route):
     if msg.text:
-        new_text = msg.text
-        for replace in route.replaces:
-            new_text = new_text.replace(replace.old_text, replace.new_text)
-
-        await bot.send_message(route.target_id, new_text)
+        new_text = msg.html_text
+        # for replace in route.replaces:
+        #     new_text = new_text.replace(replace.old_text, replace.new_text)
+        await bot.send_message(route.target_id, new_text, reply_markup=msg.reply_markup)
+    elif msg.photo:
+        new_caption = msg.html_text if msg.caption else None
+        await bot.send_photo(route.target_id, msg.photo[-1].file_id, new_caption, reply_markup=msg.reply_markup)
--- handlers/cross_post_msg.py
+++ handlers/cross_post_msg.py
@@ -4,8 +4,8 @@ import api
 from loader import dp
 
 
-@dp.message_handler()
-@dp.channel_post_handler()
+@dp.message_handler(content_types='any')
+@dp.channel_post_handler(content_types='any')
 async def cross_post_msg(msg: types.Message):
     for route in api.get_routes_by_source(msg.chat.id):
         await api.send_edited_copy_of_msg(msg, route)
